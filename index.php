abce214b7b0d99d7999b74bf1796652d7cb85727
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-16 16:48:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ $requestXmlBody .= '<GetOrdersRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
 $requestXmlBody .= '<DetailLevel>ReturnAll</DetailLevel>';
 $requestXmlBody .= '<OrderIDArray> Seller <OrderID> All </OrderID></OrderIDArray>';
 $requestXmlBody .= "<CreateTimeFrom>$CreateTimeFrom</CreateTimeFrom><CreateTimeTo>$CreateTimeTo</CreateTimeTo>";
+$requestXmlBody .= '<OrderRole>Seller</OrderRole><OrderStatus>Active</OrderStatus>';
 $requestXmlBody .= "<RequesterCredentials><eBayAuthToken>$userToken</eBayAuthToken></RequesterCredentials>";
 $requestXmlBody .= '</GetOrdersRequest>';
 print_r($requestXmlBody);
