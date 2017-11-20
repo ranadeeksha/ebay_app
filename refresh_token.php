51e0a5fb99e5e9c1588b4e752d5c4c69147047ad
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-20 18:22:38 +0530

    Add files via upload

---
+++ refresh_token.php
@@ -0,0 +1,33 @@
+<?php 
+
+$refresh_token = file_get_contents('refresh_token.txt');
+include_once 'keys.php';
+$auth_code=$Authorization_code;
+$curl_params="grant_type=refresh_token&refresh_token=$refresh_token&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly";
+
+$curl_handle = curl_init();
+curl_setopt($curl_handle, CURLOPT_URL, $ebay_token_url);
+curl_setopt($curl_handle, CURLOPT_HTTPHEADER, array(
+  "Content-Type: application/x-www-form-urlencoded",
+  "Authorization: Basic " . base64_encode($ebay_client_id . ':' . $ebay_client_secret)
+  ));
+curl_setopt($curl_handle, CURLOPT_POST, true);
+curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $curl_params);
+curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
+
+$curl_response = curl_exec($curl_handle);
+$user_access_token_array = json_decode($curl_response,true);
+// print_r($user_access_token_array);
+$curl_error = curl_errno($curl_handle);
+if ($curl_error) {
+  $curl_error = curl_error($curl_handle);
+  die();
+}
+
+$user_access_token_array['access_token'];
+$file = fopen("access_token.txt","w");
+fwrite($file,$user_access_token_array['access_token']);
+fclose($file);
+
+?>
