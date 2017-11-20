2c78613205645b69904c63ada3e18d5e49eaf1aa
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-20 18:36:31 +0530

    Update keys.php

--- keys.php
+++ keys.php
@@ -3,6 +3,6 @@
 $ebay_token_url = 'https://api.ebay.com/identity/v1/oauth2/token';
 $ebay_client_id = 'deekshar-OrderDis-PRD-4134e8f72-9b7ef546';
 $ebay_client_secret = 'PRD-134e8f72e55b-f2f6-4e5d-9b99-8b3f';
-$ebay_redirect_uri = 'Atsushi_Kurihar-AtsushiK-Etoren-noiupgut';
+$ebay_redirect_uri = 'deeksha_rana-deekshar-OrderD-zcgnjpjns';
 
 ?>
