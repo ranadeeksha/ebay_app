5b05ef843f32a3bc36e3ef5626dce4c72e444d2f
ip-d <ip-d@ipd-desktop>
2017-11-20 18:07:45 +0530

    ebay

--- ebay_auth/keys.php
+++ ebay_auth/keys.php
@@ -1,8 +1,9 @@
 <?php 
 
 $ebay_token_url = 'https://api.ebay.com/identity/v1/oauth2/token';
-$ebay_client_id = 'deekshar-OrderDis-PRD-4134e8f72-9b7ef546';
-$ebay_client_secret = 'PRD-134e8f72e55b-f2f6-4e5d-9b99-8b3f';
+$ebay_client_id = 'AtsushiK-Etoren-PRD-e132041a0-f9c6b726';
+$ebay_client_secret = 'PRD-132041a01524-2ea9-4998-93e1-89ef';
 $ebay_redirect_uri = 'Atsushi_Kurihar-AtsushiK-Etoren-noiupgut';
+
 
 ?>
