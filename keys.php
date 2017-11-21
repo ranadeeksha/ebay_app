ddb3f8cc73ec2509abc6be37db78ca96528fa8d6
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-21 15:21:34 +0530

    Update keys.php

--- keys.php
+++ keys.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
+$compatabilityLevel= 681;
 $ebay_token_url = 'https://api.ebay.com/identity/v1/oauth2/token';
-$ebay_client_id = 'deekshar-OrderDis-PRD-4134e8f72-9b7ef546';
-$ebay_client_secret = 'PRD-134e8f72e55b-f2f6-4e5d-9b99-8b3f';
-$ebay_redirect_uri = 'deeksha_rana-deekshar-OrderD-zcgnjpjns';
-
+$appID= 'deekshar-OrderDis-PRD-4134e8f72-9b7ef546';
+$certID = 'PRD-134e8f72e55b-f2f6-4e5d-9b99-8b3f';
+$runame = 'deeksha_rana-deekshar-OrderD-zcgnjpjns';
+$devId='38bd1407-6150-4a2a-87ea-563d0dd48b61';
 ?>
