5425b8164a70c41a83f349baf95cfff2d063376b
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-21 15:34:57 +0530

    Update keys.php

--- keys.php
+++ keys.php
@@ -6,4 +6,5 @@ $appID= 'deekshar-OrderDis-PRD-4134e8f72-9b7ef546';
 $certID = 'PRD-134e8f72e55b-f2f6-4e5d-9b99-8b3f';
 $runame = 'deeksha_rana-deekshar-OrderD-zcgnjpjns';
 $devId='38bd1407-6150-4a2a-87ea-563d0dd48b61';
+$serverUrl = 'https://api.sandbox.ebay.com/ws/api.dll';
 ?>
