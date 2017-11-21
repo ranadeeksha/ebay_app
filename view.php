5d5f1e126a97f27bce81fbe5596250a3a0aec8a9
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-21 13:02:20 +0530

    Update view.php

--- view.php
+++ view.php
@@ -2,7 +2,7 @@
 
 /*  © 2013 eBay Inc., All Rights Reserved */ 
 /* Licensed under CDDL 1.0 -  http://opensource.org/licenses/cddl1.php */
-require 'databaseconfig.php';
+
 
 header("Content-Type: text/plain; charset=UTF-8");
 //$response = simplexml_import_dom($responseDoc);
