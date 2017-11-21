7c60ef59a517708bee011b9b2e4ee288445252b9
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-21 11:56:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 
 if (!isset($_GET['etor'])) {
-	die('Unauthorised Access');
+	die('Unauthorised Access value');
 }
 
 
 ?>
 
-<a href="https://signin.ebay.com/authorize?client_id=AtsushiK-Etoren-PRD-e132041a0-f9c6b726&response_type=code&state=etor&redirect_uri=Atsushi_Kurihar-AtsushiK-Etoren-noiupgut&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly">Get Outh Token</a>
+<a href="https://signin.sandbox.ebay.com/authorize?client_id=deekshar-OrderDis-SBX-6134e8f72-71469deb&response_type=code&redirect_uri=deeksha_rana-deekshar-OrderD-zcgnjpjns&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly">Get Outh Token</a>
