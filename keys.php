5f0a854a8449de4fd6d92a75238c3fd65a25410d
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-22 12:07:44 +0530

    Update keys.php

--- keys.php
+++ keys.php
@@ -2,8 +2,8 @@
 
 $compatabilityLevel= 681;
 $ebay_token_url = 'https://api.sandbox.ebay.com/identity/v1/oauth2/token';
-$appID= 'deekshar-OrderDis-PRD-4134e8f72-9b7ef546';
-$certID = 'PRD-134e8f72e55b-f2f6-4e5d-9b99-8b3f';
+$appID= 'deekshar-OrderDis-SBX-6134e8f72-71469deb';
+$certID = 'SBX-134e8f724d4b-045d-4c91-b24f-d5c5';
 $runame = 'deeksha_rana-deekshar-OrderD-lgaadpmlr';
 $devId='38bd1407-6150-4a2a-87ea-563d0dd48b61';
 $serverUrl = 'https://api.sandbox.ebay.com/ws/api.dll';
