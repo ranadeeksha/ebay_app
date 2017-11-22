c3aa764f37b704f3036ed5a62d8197079546e6d1
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-22 11:26:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 ini_set("display_errors", 1);
-// if (!isset($_GET['etor'])) {
-// 	die('Unauthorised Access value');
-// }
+if (!isset($_GET['etor'])) {
+	die('Unauthorised Access value');
+}
 ?>
 <a href="index2.php">order display</a>
 <a href="https://signin.sandbox.ebay.com/authorize?client_id=deekshar-OrderDis-SBX-6134e8f72-71469deb&response_type=code&state=etor&redirect_uri=deeksha_rana-deekshar-OrderD-lgaadpmlr&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly">Get Outh Token</a>
