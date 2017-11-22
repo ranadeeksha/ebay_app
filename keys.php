b6850858c311132f6a9771d5c437a4d2472ff1cc
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-11-22 12:56:54 +0530

    Update keys.php

--- keys.php
+++ keys.php
@@ -1,11 +1,11 @@
 <?php
 
-$compatabilityLevel= 681;
+$compatabilityLevel= 967;
 $ebay_token_url = 'https://api.sandbox.ebay.com/identity/v1/oauth2/token';
 $appID= 'deekshar-OrderDis-SBX-6134e8f72-71469deb';
 $certID = 'SBX-134e8f724d4b-045d-4c91-b24f-d5c5';
 $runame = 'deeksha_rana-deekshar-OrderD-lgaadpmlr';
 $devId='38bd1407-6150-4a2a-87ea-563d0dd48b61';
 $serverUrl = 'https://api.sandbox.ebay.com/ws/api.dll';
-$userToken=" v^1.1#i^1#f^0#I^3#r^1#p^3#t^Ul4xMF8xMDpBMjQ4MzFFRjE2NjMzN0RBRDBBMjVEODdGMTQwMDE0QV8xXzEjRV4xMjg0";
+$userToken="v^1.1#i^1#f^0#I^3#r^1#p^3#t^Ul4xMF8xMDpBMjQ4MzFFRjE2NjMzN0RBRDBBMjVEODdGMTQwMDE0QV8xXzEjRV4xMjg0";
 ?>
